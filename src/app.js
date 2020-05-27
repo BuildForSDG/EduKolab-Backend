b856f4adea4e5c23036c6696cf52b338160663f2
Okechukwu Okonwanji <ookonwanji@seamfix.com>
2020-05-27 03:00:11 +0100

    First Commit: This is a base commit with just base server command.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,48 @@
-const app = async () => '#BuildforSDG';
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { body } = require('express-validator');
 
-export default app;
+const app = express();
+
+const authRoutes = require('./routes/auth');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+}@cluster0-pcodi.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// eslint-disable-next-line consistent-return
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (request.method === 'OPTIONS') {
+    response.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return response.status(200).json({});
+  }
+  next();
+});
+
+app.use('/auth', authRoutes);
+
+app.use((error, request, response) => {
+  const status = error.statusCode || 500;
+  const errorMessage = error.message;
+  const errorData = error.data;
+  response.status(status).json({ message: errorMessage, data: errorData });
+});
+
+module.exports = app;
