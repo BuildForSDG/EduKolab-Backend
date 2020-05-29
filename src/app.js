27f6031fab6b8f133da26107663d3eae73739d5e
Okechukwu Okonwanji <ookonwanji@seamfix.com>
2020-05-29 13:13:14 +0100

    User registration api comppleted with complete functionalities.

--- src/app.js
+++ src/app.js
@@ -1,49 +1,67 @@
 const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const { body } = require('express-validator');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const compression = require('compression');
+const cors = require('cors');
+const passport = require('passport');
+const httpStatus = require('http-status');
+const config = require('./config/config');
+const morgan = require('./config/morgan');
+const { jwtStrategy } = require('./config/passport');
+const { authLimiter } = require('./middlewares/rateLimiter');
+const routes = require('./routes/v1');
+const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
 
 const app = express();
 
-const authRoutes = require('./routes/auth');
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
 
-const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
-  process.env.MONGO_PASSWORD
-}@cluster0-pcodi.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+// set security HTTP headers
+app.use(helmet());
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+app.use(mongoSanitize());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// jwt authentication
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
+
+// limit repeated failed requests to auth endpoints
+if (config.env === 'production') {
+  app.use('/v1/auth', authLimiter);
+}
+
+// v1 api routes
+app.use('/v1', routes);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(body());
+// convert error to ApiError, if needed
+app.use(errorConverter);
 
-// eslint-disable-next-line consistent-return
-app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*');
-  response.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (request.method === 'OPTIONS') {
-    response.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return response.status(200).json({});
-  }
-  next();
-});
-
-app.use('/auth', authRoutes);
-
-app.use((error, request, response) => {
-  const status = error.statusCode || 500;
-  const errorMessage = error.message;
-  const errorData = error.data;
-  response.status(status).json({ message: errorMessage, data: errorData });
-});
+// handle error
+app.use(errorHandler);
 
 module.exports = app;
